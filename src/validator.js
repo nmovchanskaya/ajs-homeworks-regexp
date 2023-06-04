7c11bf2866b8c5365d85eb46a32907e56527c9f2
nika.movchanskaya <nika.movchanskaya@gmail.com>
2023-06-04 17:29:17 +0200

    regexp to blocks

--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,28 @@
 export default class Validator {
+  constructor() {
+    this.allowLatinLetters = '[a-zA-Z]+';
+    this.allowThreeNumbers = '\\d{0,3}';
+    this.allowLettersUnderscore = '\\w';
+    this.allowDashes = '-';
+    this.exeptThreeNumbersInRow = '(?!.*\\d{4})';
+  }
+  
   validateUsername(name) {
-    return /^(?!.*\d{4})[a-zA-Z]+[\w\d{0,3}_-]*[a-zA-Z]$/.test(name);
+    // return /^(?!.*\d{4})[a-zA-Z]+[\w\d{0,3}_-]*[a-zA-Z]$/.test(name);
+    const regexpStr = '^' 
+        + this.exeptThreeNumbersInRow 
+        + this.allowLatinLetters 
+        + '['
+        + this.allowLettersUnderscore
+        + this.allowThreeNumbers
+        + this.allowDashes
+        + ']*'
+        + this.allowLatinLetters
+        + '$';
+    const regexpObj = new RegExp(regexpStr);
+    return regexpObj.test(name); 
   }
 }
 
 const v = new Validator();
+// console.log(v.validateUsername('abc3_fg-1234a'));
